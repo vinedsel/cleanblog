fd1eeb18c23e0f6c52841b23368b280a2545c088
Raymond Gregory <vinedsel@gmail.com>
2020-03-16 21:32:54 -0500

    Added engines to package.json file, and modified listening port for heroku

--- index.js
+++ index.js
@@ -62,8 +62,11 @@ app.post('/users/login', redirectIfAuthenticatedMiddleWare, loginUserController)
 app.get('/auth/logout', logoutController);
 app.use((req,res)=> res.render('notfound'));
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 4000;
+}
 
-
-app.listen(4000, ()=>{
+app.listen(port, ()=>{
     console.log('App listening on port 4000');
 });
